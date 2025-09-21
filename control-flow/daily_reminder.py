320fcf1689d69f094cc928c205bd9b3e35c7a2a5
Mcdharmie <damilola.afolayan07@gmail.com>
2025-09-21 09:47:26 -0700

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,7 +2,7 @@ task = input("Enter your task: ")
 
 priority = input("Priority (high/medium/low): ")
 
-time_bound = input("Is it time-bound? (yes/no) ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
@@ -15,7 +15,7 @@ match priority:
         reminder = f"'{task}' is a task with unspecified priority "
 
 
-if time == "yes":
+if time_bound == "yes":
     reminder += "that requires immediate attention today!"
 else:
     reminder += "Consider completing it when you have free time."
